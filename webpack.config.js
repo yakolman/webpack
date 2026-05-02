f7a827e073c109905cf84dbf6e17eac2f685a0d6
kolman <ya.lks2144@yandex.ru>
2026-05-02 14:43:47 +0300

    Fixes

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require ('mini-css-extract-plugin')
 
 module.exports = {
+    mode: 'production',
     module: {
         rules: [
             {
